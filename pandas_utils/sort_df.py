5db5480621d4b51c43a67d20351559cfa1115eaf
Cristina Sisu <cristinaYale@cristinas-air.wireless.yale.internal>
2015-09-24 16:13:26 -0400

    added function to sort a data frame in ascending or descending order from 1..n columns.

--- pandas_utils/sort_df.py
+++ pandas_utils/sort_df.py
@@ -3,6 +3,7 @@ __author__ = 'Ed Cannon'
 import pandas as pd
 from filter_df import load_csv
 import sys
+from collections import OrderedDict
 
 def sort_by_one_column(df,col,ascending=False):
     '''
@@ -14,7 +15,23 @@ def sort_by_one_column(df,col,ascending=False):
     '''
     return df.sort([col], ascending=ascending)
 
+def sort_by_multiple_columns(df,cols_order):
+    '''
+    Sorts a data frame on multiple columns
+    :param df: input data frame to sort
+    :param cols_order: sorted dictionary of columns and whether to sort
+    in ascending/descending fashion
+    :return: sorted data frame
+    '''
+    if len(df.columns) >= len(cols_order.items()):
+        return df.sort(cols_order.keys(), ascending=cols_order.values())
+    else:
+        print "number of columns in ordered dictionary > cols in data frame!"
+        return df
+
 if __name__ == '__main__':
     df = load_csv(sys.argv[1])
-    #df2 = sort_by_one_column(df,'sepal length')
-    #print df2.head()
+    #od = OrderedDict()
+    #od.__setitem__("sepal length",0)
+    #od.__setitem__("sepal width",0)
+    #res = sort_by_multiple_columns(df,od)
