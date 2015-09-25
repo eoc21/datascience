1da88f850f3ad45eac61321fbe13a96e7955a983
Cristina Sisu <cristinaYale@cristinas-air.wireless.yale.internal>
2015-09-25 10:58:14 -0400

    added simple groupby example, for a single column, with option to specify whether to sum, get mean, standard deviation, count or describe the data.

---
+++ pandas_utils/groupby_df.py
@@ -0,0 +1,30 @@
+__author__ = 'Ed Cannon'
+
+import pandas as pd
+from filter_df import load_csv
+import sys
+
+def groupby_single_column(df,col,oper):
+    '''
+
+    :param df: input data frame
+    :param col: column to group by
+    :param oper: operation to apply to groupby
+    :return: groupy data frame
+    '''
+    if oper == 'sum':
+        return df.groupby(col).sum()
+    elif oper == 'mean':
+        return df.groupby(col).mean()
+    elif oper == 'std':
+        return df.groupby(col).std()
+    elif oper == 'count':
+        return df.groupby(col).count()
+    elif oper == 'describe':
+        return df.groupby(col).describe()
+    else:
+        return df
+
+if __name__ == '__main__':
+    df = load_csv(sys.argv[1])
+    groupby_single_column(df,'class')
